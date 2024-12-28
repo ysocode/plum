112b2a4cb183c481a42fbde857658bd0e01782ab
Yuri Sales de Oliveira <yusadeol@gmail.com>
2024-12-28 00:39:57 -0300

    First commit

---
+++ src/PlumServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace YSOCode\Plum;
+
+use Illuminate\Support\ServiceProvider;
+
+class PlumServiceProvider extends  ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
